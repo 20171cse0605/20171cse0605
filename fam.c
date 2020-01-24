80f700efc4d78797c87e6f260f007165e2f86b54
20171cse0605 <60249500+20171cse0605@users.noreply.github.com>
2020-01-24 10:57:08 +0530

    Add files via upload

---
+++ fam.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+struct student
+{
+	char name[50];
+	int age;
+	float marks;
+}s;
+void main()
+{
+	printf("enter the stud i8nfo\n");
+	printf("enter nam\n");
+	gets(s.name);
+	printf("enter the age\n");
+	scanf("%d",&s.age);
+	printf("enter the marks\n");
+	scanf("%f",&s.marks);
+	printf("the student details are\n");
+	printf("name=%s\nage=%d\nmarks=%f\n",s.name,s.age,s.marks);
+}
